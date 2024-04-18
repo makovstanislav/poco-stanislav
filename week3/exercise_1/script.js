24ef89e5644c6eab646279757e1a74a675185074
Stanislav <stanislav.makov@powercoders.org>
2024-04-18 13:26:14 +0200

    Setup an initial project structure

---
+++ week3/exercise_1/script.js
@@ -0,0 +1,11 @@
+function sortAndDisplayDigits() {
+    const input = document.getElementById('digitInput').value;
+    if (!input) {
+        alert('Please enter some digits.');
+        return;
+    }
+    const digits = input.split('').map(Number);
+    digits.sort((a, b) => a - b);
+    const sortedDigits = digits.join('');
+    document.getElementById('result').textContent = sortedDigits;
+}
