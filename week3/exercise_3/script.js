a2b88237f366ce3b369631da253cc1ba80f28d7a
Stanislav <stanislav.makov@powercoders.org>
2024-04-19 09:31:30 +0200

    Add functionality

--- week3/exercise_3/script.js
+++ week3/exercise_3/script.js
@@ -1,15 +1,17 @@
-let sum = 0
-
-function addAndDisplayNumbers() {
+function substractAndDisplayNumbers() {
     const input = document.getElementById('numbersInput').value
     if (!input) {
-        alert('Please enter a number.')
+        alert('Please enter numbers.')
         return
     }
     
-    // add input to a number
-    sum = sum + Number(input)
+    // convert input to numbers array
+    const numbers = input.split(',').map(Number)
+    
+    // calculate the delta
 
+    let delta = Math.max(...numbers) - Math.min(...numbers)
+   
     // display
-    document.getElementById('result').textContent = sum
+    document.getElementById('result').textContent = delta 
 }
