6684f5ad83eb15ca8dbe7b9983a2465c3622f426
Stanislav <stanislav.makov@powercoders.org>
2024-04-23 09:48:14 +0200

    Add functionality with toString

---
+++ week4/js_conditions_and_loops/script.js
@@ -0,0 +1,9 @@
+// convert integer into binary
+
+function decimalToBinary(decimalNumber) {
+    return decimalNumber.toString(2)
+}
+
+const decimalNum = 33;
+const result = decimalToBinary(decimalNum);
+console.log(result)
