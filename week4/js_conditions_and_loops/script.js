6cdc5b02f8201a3b4e187b52aabd74edba2d0394
Stanislav <stanislav.makov@powercoders.org>
2024-04-23 12:42:57 +0200

    Add loops

--- week4/js_conditions_and_loops/script.js
+++ week4/js_conditions_and_loops/script.js
@@ -1,49 +1,14 @@
 // convert Integer into Binary String
 
-// pro version 
-/* let input = 33;
-i = 0;
-let outputString = '';
+let input = Number(prompt("Enter a number"))
+let outputString = ''
 
-do {
-    let zeroOrOne = input % 2;
-    outputString = `${zeroOrOne}${outputString}`
-    input /= 2
-} while (input > 1)
-
-
-console.log(outputString) */
-
-// loop version
-function sumArray(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
-    }
-
-    return sum
+while (input !== 0) {
+    outputString = (input % 2).toString() + outputString
+    input = Math.floor(input / 2)
 }
 
-function decimalToBinaryHonest(decimalNumber) {
-    let remainedDecimal = decimalNumber
-    let outputString = ''
-    let resultArr = []
-    let resultArraySum = sumArray(resultArr)
-
-    // record the remainder
-    for (let i = 0; resultArraySum != remainedDecimal; i++) {
-        remainedDecimal = Math.floor(remainedDecimal / 2)
-        outputString = outputString.concat((remainedDecimal % 2).toString)
-        resultArr.push(2 ** i)
-    }
-
-    return outputString
-    
-}
-
-const decimalNum = 33;
-const resultHonest = decimalToBinaryHonest(decimalNum);
-console.log('Honest: ' + resultHonest) 
+alert("The binary string is " + outputString) 
 
 
 // convert Binary String to Integer
