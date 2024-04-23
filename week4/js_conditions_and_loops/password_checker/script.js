7440cf5c29c6fed093f7912686818e1d79d2b336
Stanislav <stanislav.makov@powercoders.org>
2024-04-23 16:52:34 +0200

    Phenomenal commit

--- week4/js_conditions_and_loops/password_checker/script.js
+++ week4/js_conditions_and_loops/password_checker/script.js
@@ -1,4 +1,5 @@
-let input = prompt('Please enter your password')
+let input = prompt('Please enter your fantastic password')
+// let input = '7passwW7!85'
 
 /* 
 - upper
@@ -7,20 +8,28 @@ let input = prompt('Please enter your password')
 - no spaces
 */
 
+
 let containsUpperCase = false
 let containsNumber = false
 let isGoodLength = false
-let noSpace = true
+let hasSpace = false
+let containSpecialChars = false
+
+const specialChars = '!@#$%^&*()_-+='
+const numbers = '1234567890'
+const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 for (let i=0; i < input.length; i++) {
     let char = input[i]
-    if (char === char.toUpperCase()) {
+    if (upperCaseLetters.includes(char)) {
         containsUpperCase = true
-    } else if (isNaN(parseFloat(char))) {
+    } else if (numbers.includes(char)) {
         containsNumber = true
-    } else if (char === ' ') {
+    } else if (specialChars.includes(char)) {
+        containSpecialChars = true
+    } else if (char == ' ') {
+        hasSpace = true
         alert('Please don\'t use a space' )
-        break
     }
 }
 
@@ -29,13 +38,15 @@ if (input.length >= 8 && input.length <= 12) {
     isGoodLength = true
 }
 
-if (containsUpperCase && containsNumber && isGoodLength && noSpace) {
+
+console.log(containsUpperCase)
+console.log(containsNumber)
+console.log(isGoodLength)
+console.log(hasSpace)
+
+if (containsUpperCase && containsNumber && isGoodLength && hasSpace === false && containSpecialChars) {
     alert('Fantastic password')
 } else {
-    console.log(containsUpperCase)
-    console.log(containsNumber)
-    console.log(isGoodLength)
-    console.log(noSpace)
     alert('Invalid input')
 }
 
