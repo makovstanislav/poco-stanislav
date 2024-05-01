62ad0a9deb62436f7c404b6be29ea0ab6c77c154
Stanislav <stanislav.makov@powercoders.org>
2024-05-01 15:52:09 +0200

    Find a greater number

---
+++ week5/programming_and_algorithms/which_number_is_bigger/plan.md
@@ -0,0 +1,4 @@
+# Which number is bigger?
+
+https://bootcamp.powercoders.org/05-01-algorithms-programming-principles#/40
+
---
+++ week5/programming_and_algorithms/which_number_is_bigger/script.js
@@ -0,0 +1,11 @@
+let a = 57
+let b = 56
+
+if ( a === b) {
+    console.log(`The numbers ${a} and ${b} are equal.`)
+} else if (a > b) {
+    console.log(`The greater number of ${a} and ${b} is ${a}.`)
+} else {
+    console.log(`The greater number of ${a} and ${b} is ${b}.`)
+} 
+
