7b35eb7fdcffe5575c20a28409d9e0b0ab9fd345
Stanislav <stanislav.makov@powercoders.org>
2024-05-01 16:08:42 +0200

    Add initial functionality

---
+++ week5/programming_and_algorithms/the_word_translator/script.js
@@ -0,0 +1,15 @@
+
+function helloWorld(lang) {
+    
+    if (lang === 'fr') {
+        return 'Bonjour le monde'
+    } else if (lang === 'de') {
+        return 'Hallo Welt'
+    } else if (lang === 'es') {
+        return 'Hola Mundo'
+    } else {
+        return 'Hello, World'
+    }
+      
+
+}
