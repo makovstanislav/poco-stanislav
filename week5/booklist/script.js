f19f7d572a3d22f0ec48b7a8eb4253d585f019c6
Stanislav <stanislav.makov@powercoders.org>
2024-05-03 14:31:21 +0200

    Add styles

--- week5/booklist/script.js
+++ week5/booklist/script.js
@@ -2,25 +2,30 @@ const books = [
     {
         title: 'The Design of EveryDay Things',
         author: 'Don Norman',
+        imgUrl: 'https://m.media-amazon.com/images/I/619ncDeLijL._SL1500_.jpg',
         alreadyRead: false
     }, {
         title: 'The Most Human Human',
         author: 'Brian Christian',
+        imgUrl: 'https://images.thalia.media/00/-/d2bd20cc4e1b4835ad6eee81b9ab2963/the-most-human-human-taschenbuch-brian-christian-englisch.jpeg',
         alreadyRead: true
     }
 ]
 
 let booksFeed = books.map((book) => {
     return `
-        <div>
-            <p style="display:inline"><strong>Book: </strong>${book.title}</p>
-            <p style="display:inline"><strong>Author: </strong>${book.author}</p>
-        </div>
+        <li>
+            <p class="book-name"><em>${book.title}</em></p>
+            <p>by <strong>${book.author}</strong></p>
+            <img src="${book.imgUrl}" width="128">
+        </li>
         `
 }) 
 
 function displayBooksFeed() {
-    document.body.innerHTML = booksFeed.join('')
+    let ul = document.createElement("ul") 
+    ul.innerHTML = booksFeed.join('')
+    document.body.appendChild(ul)
 }
 
 displayBooksFeed()
