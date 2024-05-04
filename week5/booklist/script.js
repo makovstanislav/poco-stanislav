8339809a318de8e2f5b898e0f363b63882ddc111
Stanislav <stanislav.makov@powercoders.org>
2024-05-04 21:25:26 +0200

    Add working functionality

--- week5/booklist/script.js
+++ week5/booklist/script.js
@@ -1,6 +1,6 @@
 const books = [
     {
-        title: 'The Design of EveryDay Things',
+        title: 'The Design of EveryDay Things' ,
         author: 'Don Norman',
         imgUrl: 'https://m.media-amazon.com/images/I/619ncDeLijL._SL1500_.jpg',
         alreadyRead: false
@@ -12,20 +12,45 @@ const books = [
     }
 ]
 
-let booksFeed = books.map((book) => {
-    return `
-        <li>
-            <p class="book-name"><em>${book.title}</em></p>
-            <p>by <strong>${book.author}</strong></p>
-            <img src="${book.imgUrl}" width="128">
-        </li>
-        `
-}) 
-
-function displayBooksFeed() {
-    let ul = document.createElement("ul") 
-    ul.innerHTML = booksFeed.join('')
-    document.body.appendChild(ul)
+let booksFeed = function () {
+    let booksFeed = books.map((book, index) => {
+        return `
+            <li style="background-color: ${book.alreadyRead && '#DCE5BE'}">
+                <p class="book-name"><em>${book.title}</em></p>
+                <p>by <strong>${book.author}</strong></p>
+                <img src="${book.imgUrl}" width="128">
+                <div class="isReadWrapper">
+                    <input type="checkbox" id="${index}" name="${book.title}" ${book.alreadyRead ? 'checked' : ''} onchange="updateIsReadStyles(checked, name)"/>
+                    <label for="isRead">Mark as read</label>
+                </div>
+            </li>
+            `
+    })
+    return booksFeed
 }
 
-displayBooksFeed()
+function displayBooksFeed(feed) {
+    let main = document.querySelector('main')
+    main.firstChild && main.removeChild(main.firstChild)
+    let ul = document.createElement("ul") 
+    ul.innerHTML = feed.join('')
+    main.appendChild(ul)
+}
+
+function updateIsReadStyles(checked, name) {
+    // Update alreadyRead's value in the database 
+    for (let i = 0; i < books.length; i++) {
+        if (books[i].title === name) {
+            books[i]['alreadyRead'] = checked
+        }
+    }
+    
+    // Create new feed
+    let updatedBooksFeed = booksFeed()
+    console.log(updatedBooksFeed[1])
+
+    // Render new feed
+    displayBooksFeed(updatedBooksFeed)
+}
+
+displayBooksFeed(booksFeed())
